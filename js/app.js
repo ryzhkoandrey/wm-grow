188638f0f88da99bf6bf81fda9ca922c482dfa14
Андрей Рыжко <ryzhkoandrey@gmail.com>
2024-12-29 22:43:19 +0200

    +

--- js/app.js
+++ js/app.js
@@ -4,3 +4,12 @@ const swiper = new Swiper('.swiper', {
     speed: 1700,
     parallax: true,
 });
+
+document.querySelectorAll('.header-content h1').forEach(e => {
+    e.innerHTML = e.textContent.replace(/ (-|#|@){1}/g, s => s[1] + s[0]).replace(/(\S*)/g, m => {
+        return m.replace(/\S(-|#|@)?/g, '<span class="letter">$&</span>')
+    })
+    e.querySelectorAll('.letter').forEach(function (l, i) {
+        l.setAttribute('style', `z-index: -${i}; transition-duration: ${i / 5 + 1}s`)
+    })
+})
